ffb7eb7367ad0f2f14ca977a96b3359c888f0d7d
monkindey <monkindey@163.com>
2016-12-23 22:47:55 +0800

    my bio with react's function component

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Monkindey = props => (
+	<div>
+		<h2>Favorite</h2>
+		<ul>
+			<li>sport: Basketball</li>
+			<li>player: Stephen Curry & Steve Nash</li>
+		</ul>
+
+		<h2>Bio</h2>
+		<p>Someone, No one</p>
+	</div>
+)
